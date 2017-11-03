f1b1951269b8f9d54069f3fa9a1c62a1df90b736
Philip Mallory <philip@pmallory.com>
2017-11-03 16:05:10 -0700

    add the parser. precedence! associativity! yay!

--- src/jlox/Lox.java
+++ src/jlox/Lox.java
@@ -52,13 +52,25 @@ public class Lox {
         Scanner scanner =  new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
 
-        for (Token token : tokens) {
-            System.out.println(token);
-        }
+        Parser parser = new Parser(tokens);
+        Expr expr = parser.parse();
+
+        //Stop if there's a syntax error
+        if (hadError) return;
+
+        System.out.println(new AstPrinter().print(expr));
     }
 
     static void error(int line, String message) {
         report(line, "", message);
+    }
+
+    static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, " at '" + token.lexeme + "'", message);
+        }
     }
 
     static private void report(int line, String where, String message) {
@@ -66,5 +78,6 @@ public class Lox {
                "[line " + line + "] Error" + where + ": " + message);
         hadError = true;
     }
+
 }
 
